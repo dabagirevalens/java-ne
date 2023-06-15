0e27317266d02ddd79c446c3bc6c86c5fe501152
kamalafidele <kamalafizzet45@gmail.com>
2023-06-15 18:58:43 +0200

    initial commit

---
+++ src/main/java/rw/rca/SpringSecurityTemplate/Pojos/Request/LoginRequest.java
@@ -0,0 +1,12 @@
+package rw.rca.SpringSecurityTemplate.Pojos.Request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+}
